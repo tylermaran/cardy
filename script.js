9e99ca69721602dc5721e936e53111cc7ab71fc5
themanmaran <tyler.maran@gmail.com>
2022-03-29 15:53:51 -0400

    weird github hosting issue, prefacing serviceworker with /cardy/

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function () {
 		navigator.serviceWorker
-			.register('/serviceWorker.js')
+			.register('/cardy/serviceWorker.js')
 			.then((res) => console.log('service worker registered'))
 			.catch((err) => console.log('service worker not registered', err));
 	});
