1e43943215fe249a9ac7b2d6ffe939a635d1a746
themanmaran <tyler.maran@gmail.com>
2022-03-29 16:51:46 -0400

    fix serviceworker with netlify deploy

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function () {
 		navigator.serviceWorker
-			.register('/cardy/serviceworker.js')
+			.register('/serviceworker.js')
 			.then((res) => console.log('service worker registered'))
 			.catch((err) => console.log('service worker not registered', err));
 	});
