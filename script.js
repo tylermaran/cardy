5b20539330ae6cb6df63d1f9a292d1fc89ec582b
themanmaran <tyler.maran@gmail.com>
2022-03-29 15:57:10 -0400

    all lowercase

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', function () {
 		navigator.serviceWorker
-			.register('/cardy/serviceWorker.js')
+			.register('/cardy/serviceworker.js')
 			.then((res) => console.log('service worker registered'))
 			.catch((err) => console.log('service worker not registered', err));
 	});
